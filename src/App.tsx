6cf117d31bdd7b938fb8b2dadb10f97fd1fa82d9
yeonkp <yeonkp@amazon.com>
2025-03-28 03:52:33 +0000

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,120 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Authenticator } from "@aws-amplify/ui-react"
+import '@aws-amplify/ui-react/styles.css'
+import React from 'react';
+import { uploadData, getUrl } from 'aws-amplify/storage';
+import { StorageManager, StorageImage } from '@aws-amplify/ui-react-storage';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [file, setFile] = useState<File | undefined>(undefined);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setFile(event.target.files[0]);
+        }
+    };
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+    const fetchTodos = async () => {
+        const { data: items } = await client.models.Todo.list();
+        alert(items.length + "개의 데이터를 가져왔습니다.");
+    };
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    useEffect(() => {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }, []);
+
+    function createTodo() {
+        client.models.Todo.create({ content: window.prompt("Todo content") });
+    }
+
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id });
+    }
+
+    const handleUpload = async () => {
+        if (!file) {
+            alert('Please select a file first');
+            return;
+        }
+
+        try {
+            await uploadData({
+                path: `picture-submissions/${file.name}`,
+                data: file
+            });
+            alert('File uploaded successfully!');
+        } catch (error) {
+            console.error('Upload failed:', error);
+            alert('Upload failed');
+        }
+    };
+
+    const linkToStorageFile = async () => {
+        try {
+            const linkUrl = await getUrl({
+                path: "picture-submissions/aws_logo_smile_1200x630.png",
+            });
+            console.log('signed URL: ', linkUrl.url);
+            console.log('URL expires at: ', linkUrl.expiresAt);
+        } catch (error) {
+            console.error('Error generating URL:', error);
+        }
+    };
+
+    return (
+        <Authenticator>
+            {({signOut, user}) => (
+                <main>
+                    <h1>{user?.signInDetails?.loginId}'s todos</h1>
+                    <button onClick={createTodo}>+ new</button>
+                    <ul>
+                        {todos.map((todo) => (
+                            <li
+                                onClick={() => deleteTodo(todo.id)}
+                                key={todo.id}>{todo.content}</li>
+                        ))}
+                    </ul>
+                    <div>
+                        🥳 App successfully hosted. Try creating a new todo.
+                        <br />
+                        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                            Review next step of this tutorial.
+                        </a>
+                    </div>
+                    <div>
+                        <input type="file" onChange={handleChange}/>
+                        <button onClick={handleUpload}>
+                            Upload
+                        </button>
+                        <StorageManager
+                            acceptedFileTypes={['image/*']}
+                            path="picture-submissions/"
+                            maxFileCount={1}
+                            isResumable
+                        />
+                        <StorageImage 
+                            alt="data" 
+                            path="picture-submissions/aws_logo_smile_1200x630.png"
+                        />
+                    </div>
+                    <div>
+                        <button onClick={linkToStorageFile}>Download URL 생성하기</button>
+                    </div>
+                    <div>
+                        <button onClick={fetchTodos}>Fetch Data</button>
+                        <button onClick={signOut}>Sign out</button>
+                    </div>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "absproxy/5173",
+  server: {
+    allowedHosts: [
+      'localhost',
+      'd2uy18bs8rszpj.cloudfront.net'
+    ]
+  }
 })
